fed851c3783ecc25ba620edf23f9e06035585db4
Jostein Hellerud <skilbert@users.noreply.github.com>
2016-06-08 13:31:50 +0200

    commented

--- src/arduino/rfidv2/rfidv2.ino
+++ src/arduino/rfidv2/rfidv2.ino
@@ -1,5 +1,9 @@
 #include <SoftwareSerial.h>
 
+//Arduino code for podio.
+//koden fungerer med RFID eval fra sparkfun
+//kommuniserer med Raspberry pi over serial
+
 SoftwareSerial rfid(7, 8);
 SoftwareSerial xbee(10, 9);
 
@@ -20,7 +24,8 @@ void set_flag(void);
 int flag = 0;
 int Str1[11];
 
-//INIT
+//setup setter opp serial på riktig data rate. Både usb serial og kommunikasjon med RFID shield.
+//Vi bruker delay(10) enkelte steder fordi vi opplever at Arduino fungerer bedre med dette ved å legge inn en kort delay.
 void setup() {
   Serial.begin(9600);
   Serial.println("Start");
@@ -32,22 +37,20 @@ void setup() {
   halt();
 }
 
-//MAIN
+//loop. Programmet kjøres her i fra etter setup er ferdig. Kaller bare read_serial()
 void loop() {
   read_serial();
 }
-
+//metoden brukes ikke i den siste utgaven av prototypen, men ble brukt under testing og er defor med
 void check_for_notag() {
   seek();
   delay(10);
   parse();
   set_flag();
-  
-  
 }
 
+//Halt tag før vi begynner loop og leser fra den
 void halt() {
- //Halt tag
   rfid.write((uint8_t)255);
   rfid.write((uint8_t)0);
   rfid.write((uint8_t)1);
@@ -55,20 +58,19 @@ void halt() {
   rfid.write((uint8_t)148);
 }
 
+//Parser rfid shield etter informasjon og leser det som ligger på den
 void parse() {
-  while(rfid.available()){
-    Serial.println("hello");
+  while(rfid.available()){ //leser mens noe er tilgjengelig
     if(rfid.read() == 255){
       for(int i=1;i<11;i++){
-        Str1[i]= rfid.read();
+        Str1[i]= rfid.read(); //leser rfid inn i Str1
       }
     }
   }
 }
 
-void print_serial()
-{
-    
+//Printer resultat fra lesingen. Print = serial print. Som betyr at det sendes til Raspberry pien
+void print_serial(){
     RFIDserial = ""; // Resetter string for nye rfider kan bli brukt
     for(int i = 0; i < 4; i++){  
       int p = 8 - i;
@@ -76,20 +78,20 @@ void print_serial()
       sprintf(b, "%X", Str1[p]); //konverterer hex til string
       RFIDserial += b;  // legger neste brikke inn
     }
-    Serial.println(RFIDserial);
-    if(RFIDserial != last){
+    //Serial.println(RFIDserial);
+    if(RFIDserial != last){ //vi ønsker bare å printe hvis det er forandringer
       delay(10);
-      if(RFIDserial == "FFFFFFFFFFFFD0"){
-        Serial.println("stop " + name);
-      }else if(RFIDserial != "60FFD0"){
+      if(RFIDserial == "FFFFFFFFFFFFD0"){//hvis det er tilfellet er noe fjernet fra leseren
+        Serial.println("stop " + name); // vi sender da at vi skal stoppe denne stasjonen eller podcasten
+      }else if(RFIDserial != "60FFD0"){//default verdi vi måtte fjerne fordi det betyr at det ikke ligger noe på den
          name = RFIDserial;
-         Serial.println("start "+RFIDserial);
+         Serial.println("start "+RFIDserial); // hvis noe er lagt på betyr det at vi skal starte denne stasjonen eller podcasten
       }
-      last = RFIDserial;
+      last = RFIDserial; //oppdaterer last
     }
- // skriver ut til serial.monitor   
 }
 
+//denne metoden kjører programmet og kalles av loop(). Holder orden på rekkefølgen. Bruker enkelte delays fordi vi opplever at den fungerte bedre med disse.
 void read_serial() {
   seek();
   delay(10);
@@ -100,8 +102,8 @@ void read_serial() {
  
 }
 
+//setter RFID leseren til å søke
 void seek() {
-  //skal finne rfid-tag
   rfid.write((uint8_t)255);
   rfid.write((uint8_t)0);
   rfid.write((uint8_t)1);
@@ -109,20 +111,15 @@ void seek() {
   rfid.write((uint8_t)131);
   delay(10);
 }
-
-void set_flag()
-{
-  if(Str1[2] == 6){         // returnerer tag
+//vi sjekker resultatet etter parsingen og flagger hvis leseren har noe å lese.
+//flagget brukes ikke i siste versjon av programvaren, men har blirt brukt under testing og er derfor med fortsatt
+void set_flag(){
+  if(Str1[2] == 6){
     flag++;
   }
   if(Str1[2] == 2){           // prosesserer eller error
- 
-    
     if (newTag == false){ // sjekker om rfid ligger på shield
       flag = 0;
-      Serial.print("The RFID ");
-      Serial.print(RFIDserial);
-      Serial.println(" has left the station!");
       newTag = true;            // Flipper så ikke output skriver det samme
     }
   }
